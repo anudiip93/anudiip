cd239ae580f66830187fc311fe45441c2efd4ca7
Anudiip <107012936+anudiip93@users.noreply.github.com>
2023-12-31 20:54:27 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ShowData/>
+</template>
+
+<script>
+import ShowData from './ShowData.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    
+    ShowData
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
