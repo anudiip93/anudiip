37f95e477a1de6d73475667ba4fba5666af8f36d
Anudiip <107012936+anudiip93@users.noreply.github.com>
2023-12-31 20:53:07 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,37 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+ 
+  <!-- <a href="./components/Home.vue">Home</a>
+  <a href="./components/Products.vue">Products</a>
+  <a href="./components/ContactUs.vue">Contact Us</a> -->
+
+ <router-link to="/">Home Page</router-link>
+ <router-link to="/product">Products</router-link>
+ <router-link to="/contact">Contact Us</router-link>
+ <router-view></router-view>
+
+</template>
+
+<script>
+import { RouterLink } from 'vue-router';
+
+
+export default {
+  name: 'App',
+  components: {
+    
+    RouterLink
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
